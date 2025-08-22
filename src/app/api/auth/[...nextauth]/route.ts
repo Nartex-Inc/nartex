3bffae5f16dbd162c714178ba046904a682416f2
Nicolas Labranche <n.labranche@nartex.ca>
2025-08-22 11:47:03 -0400

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,3 @@
 // src/app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth"; // <-- Import from the new shared file
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+import { handlers } from "@/auth"; // <-- Import from the new root auth.ts file
+export const { GET, POST } = handlers;
