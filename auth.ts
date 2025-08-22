94deb57c2174197191c1766f42b3aaf517e4f58e
Nicolas Labranche <n.labranche@nartex.ca>
2025-08-22 11:55:00 -0400

    Delete auth.ts

--- auth.ts
+++
@@ -1,6 +0,0 @@
-// auth.ts
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/auth" // We will import our config
-
-// Initialize NextAuth with your configuration and export the handlers and auth function
-export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
