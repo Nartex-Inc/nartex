c79589310010590c9f3469504788290037af362d
Nicolas Labranche <n.labranche@nartex.ca>
2025-08-26 11:56:57 -0400

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,2 @@
-// src/app/api/auth/[...nextauth]/route.ts
-
-// The auth file is now at the root. We go up 5 levels to reach it.
 import { handlers } from "../../../../../auth";
-
 export const { GET, POST } = handlers;
