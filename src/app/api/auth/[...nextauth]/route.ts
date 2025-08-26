84916fe1c6652938326570d60f2053276cbc25df
Nicolas Labranche <n.labranche@nartex.ca>
2025-08-25 20:08:49 -0400

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,1 @@
-// src/app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@/auth"; // This import will now work
-export const { GET, POST } = handlers;
+export { GET, POST } from "@/auth"
