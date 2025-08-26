44100477ac35204db6dd330aba73a5b2a6aeddd2
Nicolas Labranche <n.labranche@nartex.ca>
2025-08-26 14:51:10 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 // middleware.ts
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config'; // Import the safe config
+import { authConfig } from './auth.config'; // Import the SAFE config
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  // This matcher protects all routes except for API, Next.js assets, and static files.
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
