05f4acce349ba85cd29ba5778f5c70a7a92800a8
Nicolas Labranche <n.labranche@nartex.ca>
2025-08-27 13:51:00 -0400

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,27 +1,21 @@
-// next-auth.d.ts
+// types/next-auth.d.ts
 import { DefaultSession, DefaultUser } from "next-auth";
 import { JWT as DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-  /**
-   * Extends the built-in session to include your custom properties.
-   */
-  interface Session {
-    user: {
-      id: string;
-      role: string | null | undefined;
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & {
+      id?: string;
+      role?: string | null;
       firstName?: string | null;
       lastName?: string | null;
       emailVerified?: Date | null;
-    } & DefaultSession["user"]; // Inherits name, email, image from the default session
+    };
   }
 
-  /**
-   * Extends the built-in user model.
-   */
   interface User extends DefaultUser {
-    id: string;
-    role: string | null | undefined;
+    id?: string;
+    role?: string | null;
     firstName?: string | null;
     lastName?: string | null;
     emailVerified?: Date | null;
@@ -29,12 +23,9 @@ declare module "next-auth" {
 }
 
 declare module "next-auth/jwt" {
-  /**
-   * Extends the JWT token to include your custom properties.
-   */
   interface JWT extends DefaultJWT {
-    id: string;
-    role: string | null | undefined;
+    id?: string;
+    role?: string | null;
     firstName?: string | null;
     lastName?: string | null;
     emailVerified?: Date | null;
