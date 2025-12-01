653120b3b7275b64facd9a1285e61808e901ad11
Nicolas Labranche <n.labranche@nartex.ca>
2025-12-01 12:27:02 -0500

    Update route.ts

--- src/app/api/prextra/order/route.ts
+++ src/app/api/prextra/order/route.ts
@@ -3,7 +3,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth-options";
+import { authOptions } from "@/lib/auth";
 
 export async function GET(request: NextRequest) {
   try {
